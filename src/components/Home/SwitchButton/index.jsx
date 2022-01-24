96bd499200c0e2fe77412db4df966efc716f96cf
Jihoyang0120 <jyangca@connet.ust.hk>
2022-01-24 18:19:11 +0900

    Change Switch Button

--- src/components/Home/SwitchButton/index.jsx
+++ src/components/Home/SwitchButton/index.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
-const SwitchButton = 
+const SwitchButton = ({ isBlogPage, switchPage }) => {
+  return (
+    <div className="switchButton-wrap">
+      <button className="switch-button" onClick={switchPage}>
+        {isBlogPage ? "To Post" : "To Blog"}
+      </button>
+    </div>
+  );
+};
 
+export default SwitchButton;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
-import EmptyList from '../../components/common/EmptyList';
-import BlogList from '../../components/Home/BlogList';
-import Header from '../../components/Home/Header';
-import SearchBar from '../../components/Home/SearchBar';
-import { blogList } from '../../config/data';
+import React, { useState } from "react";
+import EmptyList from "../../components/common/EmptyList";
+import BlogList from "../../components/Home/BlogList";
+import Header from "../../components/Home/Header";
+import SearchBar from "../../components/Home/SearchBar";
+import { blogList } from "../../config/data";
+import SwitchButton from "../../components/Home/SwitchButton";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(blogList);
-  const [searchKey, setSearchKey] = useState('');
+  const [searchKey, setSearchKey] = useState("");
+  const [isBlogPage, setisBlogPage] = useState(true);
+
+  //
+  const switchPage = () => {
+    isBlogPage ? setisBlogPage(false) : setisBlogPage(true);
+  };
 
   // Search submit
   const handleSearchBar = (e) => {
@@ -27,26 +34,45 @@ const Home = () => {
   // Clear search and show all blogs
   const handleClearSearch = () => {
     setBlogs(blogList);
-    setSearchKey('');
+    setSearchKey("");
   };
 
-  return (
-    <div className="main-body">
-      {/* Page Header */}
-      <Header />
+  if (isBlogPage) {
+    return (
+      <div className="main-body">
+        {/* Page Header */}
+        <Header />
 
-      {/* Search Bar */}
-      <SearchBar
-        value={searchKey}
-        clearSearch={handleClearSearch}
-        formSubmit={handleSearchBar}
-        handleSearchKey={(e) => setSearchKey(e.target.value)}
-      />
-      
-      {/* Blog List & Empty View */}
-      {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
-    </div>
-  );
+        {/* Search Bar */}
+        <SearchBar
+          value={searchKey}
+          clearSearch={handleClearSearch}
+          formSubmit={handleSearchBar}
+          handleSearchKey={(e) => setSearchKey(e.target.value)}
+        />
+        <SwitchButton isBlogPage={isBlogPage} switchPage={switchPage} />
+        {/* Blog List & Empty View */}
+        {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
+      </div>
+    );
+  } else
+    return (
+      <div className="main-body">
+        {/* Page Header */}
+        <Header />
+
+        {/* Search Bar */}
+        <SearchBar
+          value={searchKey}
+          clearSearch={handleClearSearch}
+          formSubmit={handleSearchBar}
+          handleSearchKey={(e) => setSearchKey(e.target.value)}
+        />
+        <SwitchButton isBlogPage={isBlogPage} switchPage={switchPage} />
+        {/* Blog List & Empty View */}
+        {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
+      </div>
+    );
 };
 
 export default Home;
